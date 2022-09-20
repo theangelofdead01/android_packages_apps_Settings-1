eb86b549e7b7b03691169bb0e4ab1605e8c9d651
str-br <alexfinhart@gmail.com>
2022-09-20 11:05:31 +0800

    Settings: Introduce contextual dashboard messages
    Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -52,6 +52,7 @@ import androidx.core.view.WindowInsetsCompat;
 import android.content.Context;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -74,6 +75,9 @@ import com.android.settings.homepage.contextualcards.ContextualCardsFragment;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.Utils;
 import com.android.settingslib.core.lifecycle.HideNonSystemOverlayMixin;
+
+import java.util.*;
+import java.lang.*;
 
 import com.android.settingslib.drawable.CircleFramedDrawable;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -189,6 +193,7 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         mLoadedListeners = new ArraySet<>();
         final String highlightMenuKey = getHighlightMenuKey();
         if (useStockLayout) {
+        
         mIsEmbeddingActivityEnabled = ActivityEmbeddingUtils.isEmbeddingActivityEnabled(this);
         if (mIsEmbeddingActivityEnabled) {
             final UserManager um = getSystemService(UserManager.class);
@@ -219,6 +224,57 @@ public class SettingsHomepageActivity extends FragmentActivity implements
               }
           });
         }
+
+
+	final TextView textView = findViewById(R.id.homepage_title);
+
+        switch (Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            case 5: case 6: case 7: case 8: case 9: case 10:
+       	// Generate random welcome massage as title header
+        	String[] morningMsg = getResources().getStringArray(R.array.dashboard_morning);
+        	Random genMorningMsg = new Random();
+        	int morning = genMorningMsg.nextInt(morningMsg.length-1);
+        	textView.setText(morningMsg[morning]);
+                break;
+
+            case 18: case 19: case 20: 
+        	String[] msgearlyNight = getResources().getStringArray(R.array.dashboard_early_night);
+        	Random genmsgeNight = new Random();
+        	int eNight = genmsgeNight.nextInt(msgearlyNight.length-1);
+        	textView.setText(msgearlyNight[eNight]);
+                break;
+                
+            case 21: case 22: case 23: case 0: 
+        	String[] msgNight = getResources().getStringArray(R.array.dashboard_night);
+        	Random genmsgNight = new Random();
+        	int night = genmsgNight.nextInt(msgNight.length-1);
+        	textView.setText(msgNight[night]);
+                break;
+
+             case 16: case 17:
+        	String[] msgNoon = getResources().getStringArray(R.array.dashboard_noon);
+        	Random genmsgNoon = new Random();
+        	int noon = genmsgNoon.nextInt(msgNoon.length-1);
+        	textView.setText(msgNoon[noon]);
+                break;
+
+            case 1: case 2: case 3: case 4:
+        	String[] msgMN = getResources().getStringArray(R.array.dashboard_midnight);
+        	Random genmsgMN = new Random();
+        	int mn = genmsgMN.nextInt(msgMN.length-1);
+        	textView.setText(msgMN[mn]);
+                break;
+                
+            case 11: case 12: case 13: case 14: case 15:
+        	String[] msgRD = getResources().getStringArray(R.array.dashboard_random);
+        	Random genmsgRD = new Random();
+        	int randomm = genmsgRD.nextInt(msgRD.length-1);
+        	textView.setText(msgRD[randomm]);
+                break;
+
+            default:
+                break;
+      }
 
         setupEdgeToEdge();
         mSplitController = SplitController.getInstance();
